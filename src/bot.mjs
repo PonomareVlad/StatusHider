511ae41c43b46307908a400e41b269fa27fbc144
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-06-29 18:59:59 +0500

    Initial prototype

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,17 +1,55 @@
-import {Bot} from "grammy";
+import {url, buttons, instruction, premium, success, title, ready} from "./data.mjs";
+import {Bot, InlineKeyboard, InputFile} from "grammy";
+import {hydrateReply} from "@grammyjs/parse-mode";
 
 export const {
-
-    // Telegram bot token from t.me/BotFather
     TELEGRAM_BOT_TOKEN: token,
-
-    // Secret token to validate incoming updates
     TELEGRAM_SECRET_TOKEN: secretToken = String(token).split(":").pop()
-
 } = process.env;
 
-// Default grammY bot instance
 export const bot = new Bot(token);
 
-// Sample handler for a simple echo bot
-bot.on("message:text", ctx => ctx.reply(ctx.msg.text));
+bot.use(hydrateReply);
+
+bot.callbackQuery("create", async ctx => {
+    const {id} = ctx.msg.chat;
+    const {username} = ctx.me;
+    const bot = username.replace("@", "");
+    const name = `hidden_${Date.now()}_${id}_by_${bot}`;
+    const set = new URL(name, "https://t.me/addemoji/");
+    const [{file_id: sticker}] = await Promise.all([
+        ctx.replyWithChatAction("choose_sticker"),
+        ctx.api.uploadStickerFile(id, "animated", new InputFile(url)),
+    ]);
+    const stickers = [{sticker, emoji_list: ["✨"]}];
+    const reply_markup = new InlineKeyboard().text(buttons.delete, "delete");
+    return Promise.all([
+        ctx.api.createNewStickerSet(id, name, title(bot), stickers, "animated", {sticker_type: "custom_emoji"}),
+        ctx.replyFmt(ready(set), {reply_markup}),
+        ctx.deleteMessage(),
+    ]);
+});
+
+bot.callbackQuery("delete", async ctx => {
+    const [{url} = {}] = ctx.entities("text_link");
+    const name = new URL(url).pathname.replace("/addemoji/", "");
+    return Promise.all([
+        ctx.api.deleteStickerSet(name),
+        ctx.replyFmt(success),
+        ctx.deleteMessage(),
+    ]);
+});
+
+bot.on("message", async ctx => {
+    const isPremium =
+        ctx.from.is_premium &&
+        ctx.from.id === ctx.chat.id;
+    const reply_markup = new InlineKeyboard().text(buttons.create, "create");
+    const reply = isPremium ?
+        ctx.replyFmt(instruction, {reply_markup}) :
+        ctx.replyFmt(premium);
+    return Promise.all([
+        ctx.deleteMessage(),
+        reply,
+    ]);
+});
